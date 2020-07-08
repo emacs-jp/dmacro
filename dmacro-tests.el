57fee8df46b20882decc3c2e370d0417bb0b1056
conao3 <conao3@gmail.com>
2020-07-08 18:45:17 +0900

    add misc files

---
+++ dmacro-tests.el
@@ -0,0 +1,38 @@
+;;; dmacro-tests.el --- Test definitions for dmacro  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; URL: https://github.com/conao3/dmacro.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Test definitions for `dmacro'.
+
+
+;;; Code:
+
+(require 'cort)
+(require 'dmacro)
+
+
+;; (provide 'dmacro-tests)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; dmacro-tests.el ends here
